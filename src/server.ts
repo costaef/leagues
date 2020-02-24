1dc0709deb4209329813804262e2864a54596af6
Eduardo Costa <costa.ef@gmail.com>
2020-02-24 00:36:02 -0300

    Add server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("Hello world");
+import http from 'http';
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const router = express();
+const { PORT = 3000 } = process.env;
+const server = http.createServer(router);
+
+server.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);

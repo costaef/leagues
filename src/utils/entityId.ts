4a000d803cf139be2ec207a6815f207320e24028
Eduardo Costa <costa.ef@gmail.com>
2020-02-27 14:22:56 -0300

    Fix name

--- src/utils/entityId.ts
+++ src/utils/entityId.ts
@@ -1,4 +1,4 @@
-type EntityID = {
+export type EntityId = {
   prefix: string;
   id: string;
   key: string;
@@ -16,7 +16,7 @@ export const generateEntityId = (prefix: string, length: number = 6) => {
     id = id.concat(base62Chars[random]);
   }
 
-  const entityId: EntityID = {
+  const entityId: EntityId = {
     prefix: prefix,
     id: id,
     key: `${prefix}:${id}`

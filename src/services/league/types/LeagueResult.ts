3ca0a7aa42b96f2da4f78a0fee0b522a6a6affb8
Eduardo Costa <costa.ef@gmail.com>
2020-02-28 09:35:36 -0300

    Add error option

--- src/services/league/types/LeagueResult.ts
+++ src/services/league/types/LeagueResult.ts
@@ -7,6 +7,7 @@ export enum ErrorResult {
   LeagueNotFound = 'League not found.',
   ContestantNotFound = 'Contestant not found.',
   ContestantAlreadyMember = 'Contestant is already in the league.',
+  ContestantNotAMember = 'Contestant is not a member of this league.',
   DeadlineExpired = 'Deadline expired. League entry not allowed.',
   UnknownError = 'Unknown error.'
 }
